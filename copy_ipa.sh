c7cf0747375c728a14aa831853870cd8f8bcad21
tknr <geishagirlskoume@gmail.com>
2022-08-01 18:08:25 +0900

    u

---
+++ copy_ipa.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export IFS=$'\n'
+
+IPA_LIST=''
+for SRC_IPA in `find ipa -iname "*.ipa"`
+do
+	echo ${SRC_IPA}
+	NEW_BUNDLE_NAME=`basename ${SRC_IPA} | cut -d "." -f 1 | cut -d "_" -f 1`
+	DST_IPA="public/ipa/${NEW_BUNDLE_NAME}.ipa"
+	BUNDLE_ID_PREFIX="com.example."
+	PKEY=~/.config/zsign/sample.p12
+	PROB=~/.config/zsign/sample.mobileprovision
+	source ~/.config/zsign/config
+	zsign -k ${PKEY} -m ${PROB} -n "${NEW_BUNDLE_NAME}" -b "${BUNDLE_ID_PREFIX}${NEW_BUNDLE_NAME}" -o ${DST_IPA} ${SRC_IPA}	
+	IPA_LIST+="<li><a href='${NEW_BUNDLE_NAME}.ipa'>${NEW_BUNDLE_NAME}.ipa</li>\n";
+done
+
+cat src/ipa/index.html | sed -e "s|#IPA_LIST#|${IPA_LIST}|g" > public/ipa/index.html 
